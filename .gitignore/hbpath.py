a60441a02c8735b99486088a5ed4cbc1f487fe5d
biabani <hasanbiabani@gmail.com>
2017-07-25 22:03:21 +0430

    Update and rename .gitignore to hbpath.py

---
+++ .gitignore/hbpath.py
@@ -0,0 +1,25 @@
+# Hasan Biabani 
+import os
+
+def validExt(filename,ext):
+	filename = filename.lower()
+	ext = ext.lower()
+	i=-1
+	for letter in ext[::-1]:
+		if letter == filename[i]:
+			i-=1
+			continue 
+		else:
+			return False
+	return True
+
+def imageList(dir):
+	fileList= []
+	extNames = [".jpg",".bmp",".jpeg",".png"]
+	for filename in os.listdir(dir):
+		for ext in extNames:
+			if validExt(filename,ext):
+				fileList.append(dir+"/"+filename)
+				break
+	return fileList
+	
